02fa7994d549aa867595157c61b17f677cbe4c79
Mateus Magalhães Bezerra <20221170150026@ifrn.local>
2023-10-09 14:34:15 -0300

    camerafollow

--- meu primeiro projeto/Assets/player/PLAYER1.cs
+++ meu primeiro projeto/Assets/player/PLAYER1.cs
@@ -1,16 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PLAYER1 : MonoBehaviour
 {
+    public bool noChao;
     public int velocidade = 10;
+    public int ForcaPulo = 7;
     public Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
         TryGetComponent(out rb);
         Debug.Log("Começou");
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.tag == "chão")
+        {
+            noChao = true;
+        }
     }
 
     // Update is called once per frame
@@ -21,5 +32,14 @@ public class PLAYER1 : MonoBehaviour
         float inputVertical = Input.GetAxis("Vertical");
 
         rb.AddForce(new Vector3(inputHorizontal,0,inputVertical)* velocidade);
+        if (transform.position.y <= -10)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        if(Input.GetKeyDown(KeyCode.Space) && noChao)//se apertar espaço aplica força
+        {
+            rb.AddForce(Vector3.up * ForcaPulo, ForceMode.Impulse);
+            noChao = false;
+        }
     }
 }
---
+++ meu primeiro projeto/Assets/player/camerafollow.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camerafollow : MonoBehaviour
+{
+    public Transform alvo;
+    public Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = alvo.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = alvo.position - offset;
+        
+    }
+}
